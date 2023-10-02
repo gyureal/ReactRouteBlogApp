d8310a663ce9d27b239acbfefb0b7ce917c0dc73
gyureal <yonggyu.jeong10@gmail.com>
2023-10-02 12:20:51 +0900

    fix: react-promse -> redux-promise

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import promise from 'react-promise';
+import ReduxPromise from 'redux-promise';
 
 //import App from './components/app';
 import reducers from './reducers';
 import PostIndex from './components/post_index';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
