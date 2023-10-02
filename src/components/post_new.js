3dd9408c60c7c1a3fa56ce69b14c0fa9fbc5ca71
gyureal <yonggyu.jeong10@gmail.com>
2023-10-02 16:08:47 +0900

    새 포스트 추가 리덕스 폼 추가

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
 
+    renderField(field) {
+        return (
+            <div className='form-group'>
+                <label>{field.label}</label>
+                <input 
+                    className='form-control'
+                    type="text"
+                    {...field.input}    // event?
+                />
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>
-                POST NEW!
-            </div>
+            <form>
+                <Field 
+                    label = "Title"     // trasfer in field
+                    name = "title"
+                    component={this.renderField}
+                />
+                <Field
+                    label = "Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label = "Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
         );
     }
 }
 
-export default PostNew;
+export default reduxForm({
+    form: 'PostNewForm'     // should be unique (동일한 폼 이름으로 만들면 merge 됨)
+})(PostNew);
