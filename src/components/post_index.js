db1b4c7effe6033edf889d27d7585bccc8f9dfdb
gyureal <yonggyu.jeong10@gmail.com>
2023-10-02 12:22:19 +0900

    post 리듀서 등록, post 컴포넌트에서 posts 가져오기

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component {
+    // 컴포넌트가 돔에 표시될 때 발생하는 이벤트
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
+        console.log('props post', this.props.posts);
         return (
             <div>
                 Posts Index
@@ -10,4 +18,9 @@ class PostIndex extends Component {
     }
 }
 
-export default PostIndex;
+function mapStateToProps(state) {       // redux state
+    return { posts: state.posts }
+}
+
+// 컴포넌트의 props 에 state와 actionCreator를 넣어준다.
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
