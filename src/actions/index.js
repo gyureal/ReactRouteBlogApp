66e78d8b2dd6455e5b384fdded5212004a7628c5
gyureal <yonggyu.jeong10@gmail.com>
2023-10-03 19:52:43 +0900

    post 삭제 추가

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
 export const FETCH_POST = 'fetch_post';
+export const DELTE_POST = 'delete_post';
 
 const ROOT_URL = "http://localhost:8080/api";
 
@@ -32,5 +33,15 @@ export function fetchPost(id) {
     return {
         type: FETCH_POST,
         payload: request
+    };
+}
+
+export function deletePost(id, callback) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}`)
+                        .then(callback)
+
+    return {
+        type: DELTE_POST,
+        payload: id
     };
 }
--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';      // state 가져다 써야함
-import { fetchPost } from '../actions';     // 이벤트 핸들러에 action creator 등록해야함
+import { fetchPost, deletePost } from '../actions';     // 이벤트 핸들러에 action creator 등록해야함
+import { Link } from 'react-router-dom';
 
 class PostShow extends Component {
 
     componentDidMount() {
         const {id} = this.props.match.params; // put by react-router
         this.props.fetchPost(id);
+    }
+
+    onDeleteClick() {
+        const {id} = this.props.match.params;
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -18,6 +26,11 @@ class PostShow extends Component {
 
         return (
             <div>
+                <Link to="/">back to the index</Link>
+                <button
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)}
+                > DELETE</button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <h6>content: {post.content}</h6>
@@ -27,7 +40,8 @@ class PostShow extends Component {
 }
 
 function mapStateToProps({ posts }, ownProps) {       // list of posts , PostShow 컨테이너로 전달되는 props 주입
+    console.log(posts);
     return { post: posts[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
